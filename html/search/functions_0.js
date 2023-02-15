06e15437e961e9f9c55afc65d27e4d470033e9ae
Doxygen CI <ci-arduino@invalid>
2023-02-15 19:11:26 +0000

    Deploy docs to GitHub Pages from commit e7e856ad20
    Commit: e7e856ad202720cc0ba0e3902883aa0044dc1a89
    GitHub Actions run: 4187097307

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['adafruit_5fpn532',['Adafruit_PN532',['../class_adafruit___p_n532.html#a289e2b626aa8a346e84f3a4f5de49ccd',1,'Adafruit_PN532::Adafruit_PN532(uint8_t clk, uint8_t miso, uint8_t mosi, uint8_t ss)'],['../class_adafruit___p_n532.html#aea566b77a5b2ed37bd72901f97071e89',1,'Adafruit_PN532::Adafruit_PN532(uint8_t ss)'],['../class_adafruit___p_n532.html#add07037b56e902339d1c6b459426ee04',1,'Adafruit_PN532::Adafruit_PN532(uint8_t irq, uint8_t reset, TwoWire *theWire=&amp;Wire)']]],
+  ['adafruit_5fpn532',['Adafruit_PN532',['../class_adafruit___p_n532.html#a289e2b626aa8a346e84f3a4f5de49ccd',1,'Adafruit_PN532::Adafruit_PN532(uint8_t clk, uint8_t miso, uint8_t mosi, uint8_t ss)'],['../class_adafruit___p_n532.html#aea566b77a5b2ed37bd72901f97071e89',1,'Adafruit_PN532::Adafruit_PN532(uint8_t ss)'],['../class_adafruit___p_n532.html#add07037b56e902339d1c6b459426ee04',1,'Adafruit_PN532::Adafruit_PN532(uint8_t irq, uint8_t reset, TwoWire *theWire=&amp;Wire)'],['../class_adafruit___p_n532.html#a41dae0d99fb00a2751cc1df7015b268f',1,'Adafruit_PN532::Adafruit_PN532(uint8_t reset, HardwareSerial *theSer)']]],
   ['astarget',['AsTarget',['../class_adafruit___p_n532.html#a31d480661daf7c2aa00a43b715dfafcd',1,'Adafruit_PN532']]]
 ];
